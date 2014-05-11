0c2ca3fc341d859d1156c96c487f38d2f213439c
paroza <prz202@nyu.edu>
2014-05-11 10:23:31 -0400

    gif loads dynamically and remains on the screen

--- vertical-slide/main.js
+++ vertical-slide/main.js
@@ -7,7 +7,7 @@ $(function(){
     
     var gif = false; 
     var $vsliderboxes = $('#vsliderboxes').children();
-    function autoslide(){
+    var slide = function(){
         var $active = $vsliderboxes.filter('.active');
         if ($active.index() === ($vsliderboxes.length - 1)) {
             //we need to loop back to the beginning
@@ -25,11 +25,14 @@ $(function(){
 
         //if we have finished looping through the images, -2 because you start at the last index.
          if ($active.index()=== ($vsliderboxes.length-2)){
+         clearInterval(slide); 
          loadGif();   
          gif = true; 
          }
         console.log("this is the active index"+ $active.index()); 
     }
+
+    setInterval(slide,3000); 
     
     //if (gif) 
     //stop autoslide
@@ -38,5 +41,12 @@ $(function(){
 
 function loadGif() {
     console.log("the gif is loaded"); 
+    var gif = document.getElementById("gif1"); 
+    gif.setAttribute('src', 'blue_flower.gif'); 
+    //$("#gif1").append('<img src='+ "blue_flower.gif" + '>'); 
+   // clearInterval(slide); 
+
+    //display:block
+    //display:hide; 
     //we will add the inner source of the img tag. 
 }
