e15e6a8d790fe6a9d9c54e3d837a2521fd72919b
paroza <prz202@nyu.edu>
2014-05-11 12:30:28 -0400

    now all the buttons work and disappear when you click

--- vertical-slide/main.js
+++ vertical-slide/main.js
@@ -3,38 +3,50 @@
  //$("#button").onClick(alert("you clicked")); 
 function buttonClicked() {
 
-  $(function(){ 
-    var gif = false; 
-    var $vsliderboxes = $('#vsliderboxes').children();
-    function autoslide(){
-        var $active = $vsliderboxes.filter('.active');
-        if ($active.index() === ($vsliderboxes.length - 1)) {
-            //we need to loop back to the beginning
-            var $next = $vsliderboxes.eq(0);
-            //loadGif(); 
-        } else {
-            var $next = $active.next();
+//$("#this").animate({top: '-500px'});
+//$("#this").setAttribute("style","visibility:hidden");
+//$("#this").hide(); 
+//document.getElementById("this").style.display="none";
+
+console.log("you shouldn't see the button anymore"); 
+    
+    $(function(){ 
+        var gif = false; 
+        hideButton(); 
+        var $vsliderboxes = $('#vsliderboxes').children();
+        function autoslide(){
+
+            //hideButton(); 
+          //  document.getElementById("this").style.display="none";
+
+            var $active = $vsliderboxes.filter('.active');
+            if ($active.index() === ($vsliderboxes.length - 1)) {
+                //we need to loop back to the beginning
+                var $next = $vsliderboxes.eq(0);
+                //loadGif(); 
+            } else {
+                var $next = $active.next();
+            }
+            $active.slideUp(300, function () {
+                $active.removeClass('active');
+            });
+            $next.slideUp(300, function () {
+                $next.addClass('active');
+            });
+
+            //if we have finished looping through the images, -2 because you start at the last index.
+            if ($active.index()=== ($vsliderboxes.length-2)){
+            //clearInterval(slide); 
+            loadGif();   
+            gif = true; 
+            }
+            console.log("this is the active index"+ $active.index()); 
         }
-        $active.slideUp(300, function () {
-            $active.removeClass('active');
-        });
-        $next.slideUp(300, function () {
-            $next.addClass('active');
-        });
 
-        //if we have finished looping through the images, -2 because you start at the last index.
-         if ($active.index()=== ($vsliderboxes.length-2)){
-         //clearInterval(slide); 
-         loadGif();   
-         gif = true; 
-         }
-        console.log("this is the active index"+ $active.index()); 
-    }
-
-    setInterval(autoslide,3000); 
+        setInterval(autoslide,3000); 
 
     //$("#button").click(alert("you clicked")); 
-});
+    });
 }
 
 
@@ -48,4 +60,10 @@ function loadGif() {
     //display:block
     //display:hide; 
     //we will add the inner source of the img tag. 
+}
+
+function hideButton() {
+    console.log("the button has dissapeared"); 
+    var button = document.getElementById('this'); 
+    button.style.display= 'none'; 
 }
